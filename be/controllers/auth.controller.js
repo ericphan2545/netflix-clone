ef1eedc8225d5dcead0a54f88803bb02b20b7d58
TinKot <tin2545kot@gmail.com>
2025-06-13 12:14:39 +0700

    first commit

---
+++ be/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res) {
+    res.send("Signup route is running");
+}  
+
+export async function login(req, res) {
+  res.send("Login route is running");
+}  
+
+export async function logout(req, res) {
+  res.send("Logout route is running");
+}  
