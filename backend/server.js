927ede3ddce8c5990a1f1468557af412c8284c8d
Eric Phan <ericphan2545@gmail.com>
2025-06-17 17:15:18 +0700

    Fetching movies, tv shows

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
+import tvRoutes from './routes/tv.route.js';
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -10,8 +13,11 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json()); // required to parse JSON request bodies
 
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/movie", movieRoutes)
+app.use("/api/v1/tv", tvRoutes)
 
 app.listen(PORT, () => {
     console.log('Server started at http://localhost:' + PORT);
     connectDB();
 });
+
